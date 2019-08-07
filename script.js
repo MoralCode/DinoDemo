930f2f89fe41b06cc715571f085ddd589bd6bb0d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2019-08-07 15:05:06 -0700

    make dino jump height smaller

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.onkeydown = (e) => {
 
     if (e.key =  " ") {
 
-        dinoElement.style.bottom = 200 + "px"
+        dinoElement.style.bottom = 120 + "px"
         setTimeout(() => {
             dinoElement.style.bottom = 0 + "px"
         }, 500)
